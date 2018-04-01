46e2e41b45f18fbb57b2b6bbf280d0d7c15ac462
jlloyd42 <jlloyd@csumb.edu>
2018-03-31 18:37:43 -0700

    Adding rating
    updated album schema, and added rating API

---
+++ database/API/Rating.py
@@ -0,0 +1,204 @@
+import mysql.connector
+import hashlib
+
+class Rating:
+  
+  def __init__(self):
+    ''' Create an empty object '''
+    ''' Test with test_createEmptyAccout '''
+    self.rating_id = ''
+    self.account_id = ''
+    self.date = ''
+    self.rating = ''
+    self.comment = ''
+    self.album_id = ''
+    
+  def connectToDatabase(self):
+    ''' Connect to the Database '''
+    ''' Test with test_getBy '''
+    try:
+      cnx = mysql.connector.connect(
+        user='webapp', 
+        password='centralSolutions123',
+        host='18.222.66.236', 
+        database='musicproject'
+      )
+    except mysql.connector.Error as err:
+      cnx = False
+    
+    return cnx
+    
+  def new(self, account_id, album_id, rating, comment, date):
+    ''' Populate the current object '''
+    ''' Test with test_new '''
+    success = False
+  
+    self.account_id = account_id
+    self.album_id = album_id
+    self.date = date
+    self.rating = rating
+    self.comment = comment
+    success = True
+      
+    return success
+    
+    
+  def getBy(self, column, value):
+    ''' Populate the object from the database, using rating_id '''
+    ''' Test by test_getBy* scripts '''
+    success = False
+    if column == 'rating_id':
+      cnx = self.connectToDatabase()
+      if cnx != False:
+        cursor = cnx.cursor(buffered=True)
+        query = ("SELECT * from rating where {} = %s".format(column))
+        cursor.execute(query, (value,))
+        if cursor.rowcount == 1:
+          row = cursor.fetchone()
+          self.rating_id = row[0]
+          self.account_id = row[1]
+          self.album_id = row[2]
+          self.rating = row[3]
+          self.comment = row[4]
+          self.date = row[5]
+          success = True
+        cursor.close()
+        cnx.close()
+    return success
+  
+  def toJSON(self):
+    ''' Returns a JSON string of the object. '''
+    ''' Test with test_toJSON '''
+    jsonStr = "{{ rating_id: {}, account_id: {}, album_id: {}, rating: {}, comment: '{}', date: '{}' }}".format(self.rating_id, 
+      self.account_id, self.album_id, self.rating, self.comment, self.date)
+    return jsonStr
+  
+  def addToDatabase(self):
+    ''' Adds the current object to the database '''
+    ''' Only works on new objects '''
+    ''' Test with test_SaveAndDeleteToDatabase() '''
+    success = False
+    noErrors = True
+    if self.getRatingID() == '' and self.notDuplicateComment(self.account_id, self.album_id) == True:
+      cnx = self.connectToDatabase()
+      if cnx != False:
+        cursor = cnx.cursor()
+        query = ("INSERT INTO rating (account_id, date, rating, comment, album_id) "
+                 "VALUES (%s, %s, %s, %s, %s)")
+                 
+        try:
+          cursor.execute( query, (self.account_id, self.date, self.rating, self.comment, self.album_id) )
+        except mysql.connector.Error as err:
+          noErrors = False
+        
+        if noErrors == True:
+          query = ("SELECT rating_id FROM rating WHERE account_id = %s AND album_id = %s")
+          cursor.execute( query, (self.account_id, self.album_id))
+          self.setRatingID(cursor.fetchone()[0])
+          if self.getRatingID() != '':
+            cnx.commit()
+            success = True
+
+        cursor.close()
+        cnx.close()
+
+    return success
+
+  # Accessors  
+  def getRatingID(self):
+    return self.rating_id
+    
+  def getAccountID(self):
+    return self.account_id
+    
+  def getDate(self):
+    return self.date
+    
+  def getRating(self):
+    return self.rating
+  
+  def getComment(self):
+    return self.comment
+    
+  def getAlbumID(self):
+    return self.album_id
+
+  # Mutators
+  # Test with test_Mutators()
+  def setRatingID(self, rating_id):
+    success = False
+    if (1):
+      self.rating_id = rating_id
+      success = True
+    return success
+  
+  def setAccountID(self, account_id):
+    success = False
+    if (1):
+      self.account_id = account_id
+      success = True
+    return success
+      
+  def setDate(self, date):
+    success = False
+    if (1):
+      self.date = date
+      success = True
+    return success
+  
+  def setRating(self, rating):
+    success = False
+    if (1):
+      self.rating = rating
+      success = True
+    return success
+    
+  def setComment(self, url):
+    success = False
+    if (1):
+      self.comment = url
+      success = True
+    return success
+    
+  def setAlbumID(self, album_id):
+    success = False
+    if (1):
+      self.album_id = album_id
+      success = True
+    return success
+    
+  ####
+
+  def notDuplicateComment(self, account_id, album_id):
+    ''' Checks to see if the user has commented already '''
+    ''' Test with test_notDuplicateComment '''
+    notDuplicateComment = False
+    cnx = self.connectToDatabase()
+    if cnx != False:
+      cursor = cnx.cursor(buffered=True)
+      query = ("SELECT * FROM rating WHERE account_id = %s AND album_id = %s")
+      cursor.execute(query, (account_id,album_id))
+      if cursor.rowcount < 1:
+        notDuplicateComment = True
+      cursor.close()
+      cnx.close()
+
+    return notDuplicateComment
+    
+    
+  def deleteFromDatabase(self):
+    ''' Deletes the object from the database '''
+    ''' Test with test_SaveAndDeleteFromDatabase '''
+    success = False
+    if self.getRatingID() != '':
+      cnx = self.connectToDatabase()
+      if cnx != False:
+        cursor = cnx.cursor()
+        query = ("DELETE FROM rating WHERE rating_id = %s")
+        cursor.execute(query, (self.getRatingID(),))
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        success = True
+    return success
+  
---
+++ database/API/test_rating.py
@@ -0,0 +1,89 @@
+import unittest
+from Rating import Rating
+
+class TestRating(unittest.TestCase):
+
+  def test_createEmptyRating(self):
+    object = Rating()
+    self.assertIsInstance(object, Rating)
+    self.assertEqual(object.getRatingID(), '')
+    self.assertEqual(object.getAccountID(), '')
+    self.assertEqual(object.getAlbumID(), '')
+    self.assertEqual(object.getRating(), '')
+    self.assertEqual(object.getComment(), '')
+    self.assertEqual(object.getDate(), '')
+    
+    
+  def test_new(self):
+    object = Rating()
+    object.new(3, 2, 4, "I don't actually own this album", '2018-03-02')
+    self.assertEqual(object.getRatingID(), '')
+    self.assertEqual(object.getDate(), '2018-03-02')
+    self.assertEqual(object.getAccountID(), 3)
+    self.assertEqual(object.getAlbumID(), 2)
+    self.assertEqual(object.getRating(), 4)
+    self.assertEqual(object.getComment(), "I don't actually own this album")
+    
+  def test_toJSON(self):
+    object = Rating()
+    object.new(3, 2, 4, "I don't actually own this album", '2018-03-02')
+    self.assertEqual(object.toJSON(), 
+      "{ rating_id: , account_id: 3, album_id: 2, rating: 4, comment: 'I don\'t actually own this album', "
+      "date: '2018-03-02' }" 
+    )
+    
+  def test_mutators_and_accessors(self):
+    object1 = Rating()
+    self.assertTrue(object1.setComment("I don't actually own this album"))
+    self.assertTrue(object1.setDate('2018-03-02'))
+    self.assertTrue(object1.setAccountID(3))
+    self.assertTrue(object1.setAlbumID(2))
+    self.assertTrue(object1.setRating(4))
+    
+    self.assertEqual(object1.getComment(), "I don't actually own this album")
+    self.assertEqual(object1.getDate(), '2018-03-02')
+    self.assertEqual(object1.getAccountID(), 3)
+    self.assertEqual(object1.getAlbumID(), 2)
+    self.assertEqual(object1.getRating(), 4)
+    
+  def test_getByRatingID(self):
+    object = Rating()
+    self.assertTrue(object.getBy('rating_id', 4))
+    self.assertNotEqual(object.getRatingID(), '')
+#    self.assertEqual(object.getDate(), '2018-03-31')
+    
+  def test_getByError(self):
+    object = Rating()
+    self.assertFalse(object.getBy('account_id', '2'))
+    self.assertEqual(object.getRatingID(), '')
+    
+  def test_SaveAndDeleteFromDatabase(self):
+    object1 = Rating()
+    self.assertTrue(object1.new(34, 2, 4, "I don't actually own this album", '2018-03-02'))
+    self.assertTrue(object1.addToDatabase())
+    self.assertNotEqual(object1.getRatingID(), '')
+    rating_id = object1.getRatingID()
+
+    object2 = Rating()
+    self.assertTrue(object2.getBy('rating_id', rating_id))
+    self.assertNotEqual(object2.getRatingID(), '')
+    self.assertTrue(object2.deleteFromDatabase())
+
+    object3 = Rating()
+    self.assertFalse(object3.getBy('rating_id', rating_id))
+    self.assertEqual(object3.getRatingID(), '')
+    
+  def test_duplicateRatings(self):
+    object1 = Rating()
+    self.assertTrue(object1.new(34, 2, 4, "I don't actually own this album", '2018-03-02'))
+    self.assertTrue(object1.addToDatabase())
+    self.assertNotEqual(object1.getRatingID(), '')
+
+    object2 = Rating()
+    self.assertTrue(object1.new(34, 2, 1, "I hate this album", '2017-03-02'))
+    self.assertFalse(object2.addToDatabase())
+    self.assertEqual(object2.getRatingID(), '')
+    
+    self.assertTrue(object1.deleteFromDatabase())
+
+    
