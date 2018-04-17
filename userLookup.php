eec8d6c6d0a8544cd4f495a81737af52a390dc64
AFulle <afuller@csumb.edu>
2018-04-17 10:46:41 -0700

    Update userLookup.php

--- userLookup.php
+++ userLookup.php
@@ -1,16 +1,7 @@
 <?php
     if (isset($_POST['username']))
 	{
-            $servername = "18.222.66.236";
-            $username = "webapp";
-            $password = "centralSolutions123";
-            $dbname = "musicproject";
-        
-           $dbConn new mysqli($servername, $username, $password, $dbname);
-        
-           if ($dbConn->connect_error) {
-               die("Connection failed: " . $conn->connect_error
-           }
+            require 'db_connection.php';
 	   
                 $sql = "SELECT username
         		FROM account
