12bb27955e3a7eb83ac8d42ea2196a1f71f36366
AFulle <afuller@csumb.edu>
2018-04-17 11:26:28 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -7,6 +7,6 @@
         $dbConn new mysqli($servername, $username, $password, $dbname);
         
         if ($dbConn->connect_error) {
-            die("Connection failed: " . $conn->connect_error
+            die("Connection failed: " . $conn->connect_error);
         }
 ?>
