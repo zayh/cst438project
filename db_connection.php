77cc3d067c99ba851d2a8b1fe4ff43f11b69e92b
AFulle <afuller@csumb.edu>
2018-04-17 10:43:19 -0700

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,12 @@
-
+<?php        
+        $servername = "18.222.66.236";
+        $username = "webapp";
+        $password = "centralSolutions123";
+        $dbname = "musicproject";
+        
+        $dbConn new mysqli($servername, $username, $password, $dbname);
+        
+        if ($dbConn->connect_error) {
+            die("Connection failed: " . $conn->connect_error
+        }
+?>
